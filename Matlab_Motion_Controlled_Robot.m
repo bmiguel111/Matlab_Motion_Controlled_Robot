289e0851918a56ad56823a285d3d6990f2d5cd58
Taylor <tniver1030@gmail.com>
2013-01-27 00:30:15 -0500

    Project Desctiption Added
    Added walkthrough description

---
+++ Matlab_Motion_Controlled_Robot.m
@@ -0,0 +1,14 @@
+%Take multiple pictures
+%Look for biggest changes
+%Take range for background (seperate into quadrants for different 
+%lighting?)
+%Person stands infront
+%Center person, countdown and take multiple screencaps
+%Calculate shadow difference/angle
+%Finish Calibration
+%Set boundries. If person is in starting position, dont move
+%If person is larger than boundries, move forward
+%If person is smaller than inside boundries move backwards
+%If right hand is raised, turn right
+%If left hand is raised, turn left
+%Move faster based on distance?
